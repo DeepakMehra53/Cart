3a2d32d6ca82feec324161ca64c75991cfc053fc
DeepakMehra53 <dmehra594@gmail.com>
2025-03-26 12:54:19 +0530

    All product image added

--- src/products.js
+++ src/products.js
@@ -1,12 +1,66 @@
-import 
+import imageProduct1 from './assets/images/imageProduct1'
+import imageProduct2 from './assets/images/imageProduct2'
+import imageProduct3 from './assets/images/imageProduct3'
+import imageProduct4 from './assets/images/imageProduct4'
+import imageProduct5 from './assets/images/imageProduct5'
+import imageProduct6 from './assets/images/imageProduct6'
+import imageProduct7 from './assets/images/imageProduct7'
 
 
 
 export const products =[
     {
         id:1,
-        name:"",
+        name:"Dog's Bowl 3D ",
         price:101,
-        image:
-    }
+        image:imageProduct1,
+        description:"L",
+        slug: "dog's-bowl-3d"
+
+    },
+    {
+        id: 2,
+        name: "The Hotdog Sausage Dog Lead",
+        price: 101,
+        image: imageProduct2,
+        description: "L",
+        slug: "the-hotdog-sausage-dog-lead"
+
+    },
+    {
+        id: 3,
+        name: "Dog hair colors headband",
+        price: 101,
+        image: imageProduct1,
+        description: "L",
+        slug: "dog-hair-color-headband"
+
+    },
+    {
+        id: 4,
+        name: "Blue Plaid Dog Bandana",
+        price: 101,
+        image: imageProduct4,
+        description: "L",
+        slug: "blue-plaid-dog-bandana "
+
+    },
+    {
+        id: 5,
+        name: "Personalized Pet Name Blanket for Dog's",
+        price: 101,
+        image: imageProduct5,
+        description: "L",
+        slug: "personalized-pet-name-blanket-for-dog's"
+
+    },
+    {
+        id: 6,
+        name: "Harness for Italian Greyhound ",
+        price: 101,
+        image: imageProduct7,
+        description: "L",
+        slug: "harness-for-italian-greyhound"
+
+    },
 ]
