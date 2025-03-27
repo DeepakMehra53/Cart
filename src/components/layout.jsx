1ef5d45287e1d5df70897510f340c3d3dd67688f
DeepakMehra53 <dmehra594@gmail.com>
2025-03-27 11:56:28 +0530

    Design is done

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header } from './header'
 import { Carttab } from './carttab'
+import {useSelector, UseSeletor} from 'react-redux'
 export const Layout = () => {
+  const statusTabCart = useSelector(store => store.cart.statusTabCart)
   return (
     <div className="bg-zinc-200">
-        <main className='w-[1200px] max-w-full m-auto p-5'>
+        <main className={`w-[1200px] max-w-full m-auto p-5 transform transition-transfrom duration-500 ${statusTabCart === false ?"" : "-translate-x-56"}`}>
             <Header/>
             <Outlet/>
         </main>
