e6cc8f040167aaf3715ce575a774b50240e3ed5b
DeepakMehra53 <dmehra594@gmail.com>
2025-03-27 12:46:29 +0530

    improve layout component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Header } from './header'
-import { Carttab } from './carttab'
-import {useSelector} from 'react-redux'
-export const Layout = () => {
-  const statusTabCart = useSelector(store => store.cart.statusTabCart)
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./header";
+import CartTab from "./carttab";
+import { useSelector } from "react-redux";
+
+const Layout = () => {
+  const statusTabCart = useSelector((store) => store.cart.statusTab);
   return (
     <div className="bg-zinc-200">
-        <main className={`w-[1200px] max-w-full m-auto p-5 transform transition-transfrom duration-500 ${statusTabCart === false ?"" : "-translate-x-56"}`}>
-            <Header/>
-            <Outlet/>
-        </main>
-        <Carttab/>
+      <main
+        className={`w-[1200px] max-w-full m-auto p-5 transform transition-transform duration-500
+        ${statusTabCart === false ? "" : "-translate-x-56"}`}
+      >
+        <Header />
+        <Outlet />
+      </main>
+      <CartTab />
     </div>
-  )
-}
+  );
+};
+
+export default Layout;
