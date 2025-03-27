d1f788471d1ffc675a60bdcda7857a8bbbfa4c19
DeepakMehra53 <dmehra594@gmail.com>
2025-03-27 11:33:40 +0530

    cartTab

--- src/components/carttab.jsx
+++ src/components/carttab.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { CartItem } from './cartItem'
+import CartItem from './cartItem'
 export const Carttab = () => {
   const carts = useSelector(store => store.cart.items)
+  const statusTab = useSelector(store => store.cart.statusTab)
   return (
-    <div className="fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px]">
+    <div className={`fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[60px_1fr_60px] transform ${statusTab === false ? "translate-x-full" : ""}`}>
       <h2 className="p-5 text-white text-2xl">Shopping</h2>
       <div className='p-5'>
           {carts.map((item,key)=>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState ={
-    items:[]
+    items:[],
+    statusTab: false
 }
 const cartSlice = createSlice({
     name:'cart',
@@ -22,10 +23,18 @@ const cartSlice = createSlice({
             if(quantity > 0){
                 state.items[indexProductId].quantity =quantity;
             }else{
-                delete state.items[indexProductId]
+                state.items= (state.items).filter(item => item.productId !== productId)
+            }
+        },
+        toggleStatusTab(state){
+            if(state.statusTab===false){
+                state.statusTab=true
+            }else{
+                state.statusTab = false
             }
         }
+
     }
 })
-export const {addToCart , changeQuantity} = cartSlice.actions;
+export const { addToCart, changeQuantity, toggleStatusTab } = cartSlice.actions;
 export default cartSlice.reducer;
