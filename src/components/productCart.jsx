c9ace0ea0a0ea6f8adc361f51223258f5724e91c
DeepakMehra53 <dmehra594@gmail.com>
2025-03-27 12:53:32 +0530

    Header components improve

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -1,19 +1,21 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import iconCart from "../assets/images/iconCart.png";
-import { useSelector ,useDispatch,} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../stores/cart";
-export const ProductCart = (props) => {
-  const carts = useSelector((store) => store.cart.items);
-  
-  const { id, name, price, image, slug } = props.data;
-  const dispatch = useDispatch()
-  const handleAddtoCart=()=>{
-    dispatch(addToCart({
-      productId:id,
-      quantity:1
-    }))
-  }
 
+const ProductCart = (props) => {
+  const carts = useSelector((store) => store.cart.items);
+  const { id, name, price, image, slug } = props.data;
+  const dispatch = useDispatch();
+  const handleAddToCart = () => {
+    dispatch(
+      addToCart({
+        productId: id,
+        quantity: 1,
+      })
+    );
+  };
   return (
     <div className="bg-white p-5 rounded-xl shadow-sm">
       <Link to={slug}>
@@ -28,11 +30,16 @@ export const ProductCart = (props) => {
         <p>
           $<span className="text-2xl font-medium">{price}</span>
         </p>
-        <button className="bg-orange-400 p-2 rounded-md text-sm hover:bg-orange-700 flex gap-2" onClick={handleAddtoCart}>
+        <button
+          className="bg-gray-300 p-2 rounded-md text-sm hover:bg-gray-400 flex gap-2"
+          onClick={handleAddToCart}
+        >
           <img src={iconCart} alt="" className="w-5" />
-          Add to Cart
+          Add To Cart
         </button>
       </div>
     </div>
   );
 };
+
+export default ProductCart;
